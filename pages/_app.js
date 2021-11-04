1d829767841b90615d4cbc406e2f85e9c3412a03
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-11-04 12:53:45 -0400

    adding footer and hello page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2021 Jeremy</p>
+      </footer>;
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/hello.js
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <div>Hey Hoes</div>
+};
+
+export default Hello;

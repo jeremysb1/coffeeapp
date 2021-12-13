92fc4749ff7623944b23c560c37c287d6d6b5b4a
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-12-13 15:16:41 -0500

    adding route to dynamic page w/link component

--- pages/coffee-shop/[id].js
+++ pages/coffee-shop/[id].js
@@ -8,6 +8,9 @@ const CoffeeStore = () => {
       <Link href="/">
         <a>Back to home</a>
       </Link>
+      <Link href="/coffee-shop/dynamic">
+        <a>Go to page dynamic</a>
+      </Link>
     </div>
   );
 };

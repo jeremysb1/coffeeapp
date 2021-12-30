56b20867ef15d694f5030eb2cb1ccca105df4142
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-12-30 15:19:41 -0500

    adding Link component

--- components/card.js
+++ components/card.js
@@ -1,5 +1,19 @@
-const Card = () => {
-  return <div>Card Component</div>;
+import Link from "next/link";
+import Image from "next/image";
+
+const Card = (props) => {
+  return (
+  	<Link href={props.href}>
+  	  <a>
+  	    <h2>{props.name}</h2>
+        <Image
+          alt={props.name}
+          src={props.imgUrl} 
+          width={260} 
+          height={160} />;
+      </a>
+  	</Link>
+  );   
 };
 
 export default Card;

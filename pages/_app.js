c6a67d56e2877516d8f393ae0c948d426d0bb847
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-12-30 16:33:27 -0500

    removing footer

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
-      <footer>
-        <p>© 2021 Jeremy</p>
-      </footer>;
     </div>
   );
 }

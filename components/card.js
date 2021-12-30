5103e62841505846200a8b3369b402bd42fdf796
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-12-30 16:30:32 -0500

    adding card styling

--- components/card.js
+++ components/card.js
@@ -1,16 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import styles from "./card.module.css";
+
 const Card = (props) => {
   return (
   	<Link href={props.href}>
-  	  <a>
-  	    <h2>{props.name}</h2>
-        <Image
-          alt={props.name}
-          src={props.imgUrl} 
-          width={260} 
-          height={160} />;
+  	  <a className={styles.cardLink}>
+        <div className={styles.container}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image 
+              className={styles.cardImage}
+              alt={props.name}
+              src={props.imgUrl} 
+              width={260} 
+              height={160} 
+            />
+          </div>
+        </div>
       </a>
   	</Link>
   );   

8b9df4871a422ee3b07b913ca41b780d536834b3
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-01-02 13:27:49 -0500

    refactored card component

--- components/card.js
+++ components/card.js
@@ -1,30 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
-import cls from 'classnames';
+import cls from "classnames";
 
 import styles from "./card.module.css";
 
 const Card = (props) => {
   return (
-  	<Link href={props.href}>
-  	  <a className={styles.cardLink}>
+    <Link href={props.href}>
+      <a className={styles.cardLink}>
         <div className={cls("glass", styles.container)}>
           <div className={styles.cardHeaderWrapper}>
             <h2 className={styles.cardHeader}>{props.name}</h2>
           </div>
           <div className={styles.cardImageWrapper}>
-            <Image 
-              className={styles.cardImage}
+            <Image
               alt={props.name}
-              src={props.imgUrl} 
-              width={260} 
-              height={160} 
+              className={styles.cardImage}
+              src={props.imgUrl}
+              width={260}
+              height={160}
             />
           </div>
         </div>
       </a>
-  	</Link>
-  );   
+    </Link>
+  );
 };
 
 export default Card;

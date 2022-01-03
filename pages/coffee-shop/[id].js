a0a39e060ae4a49b63646dba7c4e394e140ed359
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-01-03 12:32:21 -0500

    added getStaticProps function

--- pages/coffee-shop/[id].js
+++ pages/coffee-shop/[id].js
@@ -1,6 +1,19 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      CoffeeStore: coffeeStoresData.find(CoffeeStore => {
+        return coffeeStore.id === params.id // dynamic id
+      })
+    }
+  }
+}
+
 const CoffeeStore = () => {
   const router = useRouter();
   return (
